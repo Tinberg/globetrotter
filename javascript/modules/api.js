64b87e19cbd7e356d9dbb593c1fba608f69db1c3
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2024-02-05 12:35:48 +0100

    Organized js code for api.js auth.js my-profile.js API for my-profile finished

--- javascript/modules/api.js
+++ javascript/modules/api.js
@@ -1,7 +1,30 @@
-//--This is the Base URL--//
+
+//-- This is the Base URL --//
 const API_BASE_URL = "https://api.noroff.dev/api/v1";
 
-//--This is for register user --> register.js--//
+//------ Import --------/
+
+//-- Import the JWT to to confirm user is logged in --> moduals/utility.js --//
+import { getToken } from "./auth.js";
+
+//------ Export --------/
+
+//-- For fetch register user --> register.js
+export { registerUser };
+//-- For fetch login user --> index.js 
+export { loginUser };
+//-- For fetch spesific user info --> my-profile.js MAYBE profile.js as well??????
+export { fetchUserProfile };
+//-- For fetch posts by spesific user --> my-profile.js MAYBE profile.js as well??????
+export { fetchPostsByUserName };
+//-- For fetch edit profile media --> my-profile.js
+export { updateProfileMedia };
+
+/**
+ *
+ * @param {Object} userData
+ * @returns {Promise}
+ */
 async function registerUser(userData) {
   const response = await fetch(`${API_BASE_URL}/social/auth/register`, {
     method: "POST",
@@ -12,10 +35,12 @@ async function registerUser(userData) {
   });
   return await response.json();
 }
-
-export { registerUser };
-
-//--This is for login user --> index.js--//
+/**
+ *
+ * @param {string} email
+ * @param {string} password
+ * @returns {Promise}
+ */
 async function loginUser(email, password) {
   const response = await fetch(`${API_BASE_URL}/social/auth/login`, {
     method: "POST",
@@ -31,5 +56,74 @@ async function loginUser(email, password) {
 
   return await response.json();
 }
+/**
+ *
+ * @param {string} userName
+ * @returns {Promise}
+ */
+async function fetchUserProfile(userName) {
+  const token = localStorage.getItem("accessToken");
+  const response = await fetch(`${API_BASE_URL}/social/profiles/${userName}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Failed to fetch profile information");
+  }
+  return await response.json();
+}
+/**
+ *
+ * @param {string} userName
+ * @returns {Promise}
+ */
+async function fetchPostsByUserName(userName) {
+  const token = localStorage.getItem("accessToken");
+  const response = await fetch(
+    `${API_BASE_URL}/social/profiles/${userName}/posts`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+  return await response.json();
+}
+/**
+ *
+ * @param {string} userName
+ * @param {string} bannerUrl
+ * @param {string} avatarUrl
+ * @returns {Promise}
+ */
+async function updateProfileMedia(userName, bannerUrl, avatarUrl) {
+  const token = getToken();
+  const bodyData = JSON.stringify({
+    banner: bannerUrl,
+    avatar: avatarUrl,
+  });
 
-export { loginUser };
+  const response = await fetch(
+    `${API_BASE_URL}/social/profiles/${userName}/media`,
+    {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: bodyData,
+    }
+  );
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(`Failed to update profile media: ${errorData.message}`);
+  }
+
+  return await response.json();
+}
+
---
+++ javascript/pages/my-profile.js
@@ -0,0 +1,119 @@
+//------ Import --------/
+
+//-- Redirect function --> auth.js --//
+import { checkAuthAndRedirect } from "../modules/auth.js";
+checkAuthAndRedirect();
+//-- Api for fetch spesific user info --> api.js
+import { fetchUserProfile } from "../modules/api.js";
+//-- Api for fetch post by spesific user --> api.js
+import { fetchPostsByUserName } from "../modules/api.js";
+//-- Api for fetch edit profile media --> api.js
+import { updateProfileMedia } from "../modules/api.js";
+
+
+//-- For Displaying user info and posts, changing profile media, and it also calls the display post function--//
+document.addEventListener("DOMContentLoaded", async () => {
+  const userName = localStorage.getItem("userName");
+  if (userName) {
+    try {
+      // Fetch and display user profile information
+      const profile = await fetchUserProfile(userName);
+      document.getElementById("userName").textContent = profile.name;
+      const bannerImageElement = document.getElementById("bannerImage");
+      if (profile.banner) {
+        bannerImageElement.src = profile.banner;
+        bannerImageElement.style.display = "";
+      } else {
+        bannerImageElement.style.display = "none";
+      }
+      document.getElementById("profileImage").src =
+        profile.avatar || "/images/profileImage.jpg";
+      document.getElementById("allPosts").textContent = profile._count.posts;
+      document.getElementById("followers").textContent =
+        profile._count.followers;
+      document.getElementById("following").textContent =
+        profile._count.following;
+
+      // Fetch and display posts made by the user
+      const posts = await fetchPostsByUserName(userName);
+      displayPosts(posts);
+    } catch (error) {
+      console.error("Failed to load profile information or posts:", error);
+    }
+  } else {
+    console.error("User name not found. Redirecting to login page.");
+  }
+  //Fetch the profile media for changing banner and avatar
+  document
+    .getElementById("editProfileForm")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const resetBanner = document.getElementById(
+        "resetBannerCheckbox"
+      ).checked;
+      const resetAvatar = document.getElementById(
+        "resetAvatarCheckbox"
+      ).checked;
+
+      const currentProfile = await fetchUserProfile(userName);
+
+      let bannerUrl = resetBanner
+        ? null
+        : document.getElementById("bannerImageInput").value;
+      let avatarUrl = resetAvatar
+        ? null
+        : document.getElementById("profileImageInput").value;
+
+      bannerUrl = bannerUrl || (resetBanner ? null : currentProfile.banner);
+      avatarUrl = avatarUrl || (resetAvatar ? null : currentProfile.avatar);
+
+      try {
+        await updateProfileMedia(userName, bannerUrl, avatarUrl);
+        alert("Profile media updated successfully.");
+        window.location.reload();
+      } catch (error) {
+        console.error("Error updating profile media:", error);
+        alert("Failed to update profile media. Please try again.");
+      }
+    });
+});
+
+/**
+ * 
+ * @param {Array} posts 
+ */
+//-- For display users posts function called in DOMContentLoaded!--//
+function displayPosts(posts) {
+  const postContainer = document.getElementById("postContainer");
+  postContainer.innerHTML = "";
+
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.className = "col-lg-4 col-sm-6 mb-5";
+    postElement.innerHTML = `
+    <div class="card">
+        <div class="card-img-top-container w-100 position-relative h-0">
+            <img src="${post.media}" class="post-image card-img-top position-absolute w-100 h-100 top-0 start-0" alt="Post image">
+        </div>
+        <div class="card-body">
+            <div class="d-flex align-items-center mb-3">
+                <img src="${post.author.avatar}" class="post-profile-image rounded-circle me-3" alt="Profile image">
+                <div class="d-flex flex-column">
+                    <p class="mb-0 fs-6 fw-light">
+                        Posted by <span class="post-user-name fw-normal">${post.author.name}</span>
+                    </p>
+                    <p class="card-text fw-light">
+                        <i class="fa-solid fa-heart text-primary"></i>
+                        <span class="post-likes mx-1"><i class="fa-solid fa-heart text-primary"></i></span>|<span class="post-comments mx-1">${post._count.comments}</span> comments
+                    </p>
+                </div>
+            </div>
+        </div>
+
+      `;
+    postContainer.appendChild(postElement);
+  });
+}
+
+
