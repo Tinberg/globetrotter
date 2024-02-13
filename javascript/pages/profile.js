6bd96b894cea5769cd69bedff83a86d96b708379
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2024-02-13 12:08:30 +0100

    cursor point for post on different pages. profile almost finished
    need to make follow/unfollow work.

--- javascript/pages/profile.js
+++ javascript/pages/profile.js
@@ -7,62 +7,101 @@ checkAuthAndRedirect();
 import { fetchUserProfile } from "../modules/api.js";
 //-- Api for fetch post by spesific user --> api.js
 import { fetchPostsByUserName } from "../modules/api.js";
+//-- For formatting reaction and comment numbers to fit the layout --> utility.js --//
+import { formatCount, formatWithSuffix } from "../modules/utility.js";
 
+//-- Display profile info
 document.addEventListener("DOMContentLoaded", async () => {
-    // Parse the username from the URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const userName = urlParams.get('username');
+  const urlParams = new URLSearchParams(window.location.search);
+  const userName = urlParams.get("username");
 
-    if (!userName) {
-        console.error("User name not found in URL. Redirecting...");
-        // Redirect logic here, e.g., to a default page or showing an error message
-        return;
+  if (!userName) {
+    console.error("User name not found in URL. Redirecting...");
+    return;
+  }
+
+  try {
+    // Fetch and display user profile information for the username from URL
+    const profile = await fetchUserProfile(userName);
+    document.getElementById("userName").textContent = profile.name;
+    // Handle banner image
+    const bannerImageElement = document.getElementById("bannerImage");
+    if (profile.banner && profile.banner.url) {
+      bannerImageElement.src = profile.banner.url;
+      bannerImageElement.alt = profile.banner.alt || "Personal Banner";
+    } else {
+      bannerImageElement.src = "/images/defaultBanner.jpg";
+      bannerImageElement.alt = "Personal Banner";
     }
-
-    try {
-        // Fetch and display user profile information for the username from URL
-        const profile = await fetchUserProfile(userName);
-        document.getElementById("userName").textContent = profile.name;
-        const bannerImageElement = document.getElementById("bannerImage");
-        bannerImageElement.src = profile.banner ? profile.banner : "/images/defaultBanner.jpg";
-        document.getElementById("profileImage").src = profile.avatar ? profile.avatar : "/images/defaultAvatar.jpg";
-        document.getElementById("allPosts").textContent = profile._count.posts;
-        document.getElementById("followers").textContent = profile._count.followers;
-        document.getElementById("following").textContent = profile._count.following;
-
-        // Fetch and display posts made by this user
-        const posts = await fetchPostsByUserName(userName);
-        displayPosts(posts, profile);
-    } catch (error) {
-        console.error("Failed to load user profile or posts:", error);
-        // Handle error, e.g., by showing an error message to the user
+    // Handle profile image
+    const profileImageElement = document.getElementById("profileImage");
+    if (profile.avatar && profile.avatar.url) {
+      profileImageElement.src = profile.avatar.url;
+      profileImageElement.alt = profile.avatar.alt || "Personal profile image";
+    } else {
+      profileImageElement.src = "/images/defaultAvatar.jpg";
+      profileImageElement.alt = "Default avatar image";
     }
+    //Set postCount followers and following 
+    document.getElementById("allPosts").textContent = profile._count.posts;
+    document.getElementById("followers").textContent = profile._count.followers;
+    document.getElementById("following").textContent = profile._count.following;
 
-    // Note: Remove or adapt any code related to editing profile media, as it may not apply here
+    // Calls the fetch post by username function
+    const posts = await fetchPostsByUserName(userName);
+    displayPosts(posts, profile);
+  } catch (error) {
+    console.error("Failed to load user profile or posts:", error);
+    
+  }
+
 });
-
+//-- Display Posts from the profile user
 function displayPosts(posts, profile) {
-    const postContainer = document.getElementById("postContainer");
-    postContainer.innerHTML = "";
+  const postContainer = document.getElementById("postContainer");
+  postContainer.innerHTML = "";
 
-    posts.forEach(post => {
-        const postElement = document.createElement("div");
-        postElement.className = "col-lg-4 col-sm-6 mb-5";
-        postElement.innerHTML = `
-            <div class="card">
-                <div class="card-img-top-container w-100 position-relative h-0">
-                    <img src="${post.media}" class="post-image card-img-top position-absolute w-100 h-100 top-0 start-0" alt="Post image">
-                </div>
-                <div class="card-body">
-                    <div class="d-flex align-items-center mb-3">
-                        <img src="${profile.avatar || "/images/defaultAvatar.jpg"}" class="post-profile-image rounded-circle me-3" alt="Profile image">
-                        <div class="d-flex flex-column">
-                            <p class="mb-0 fs-6 fw-light">Posted by <span class="post-user-name fw-normal">${profile.name}</span></p>
-                            <p class="card-text fw-light"><i class="fa-solid fa-heart text-primary"></i> <span class="mx-2">|</span> <span class="post-likes mx-1">${post._count.comments || 0}</span> comments</p>
-                        </div>
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.className = "col-lg-4 col-sm-6 mb-5";
+    postElement.style.cursor = "pointer";
+
+    const postMediaUrl = post.media?.url || "/images/no-image.png";
+    const postMediaAlt = post.media?.alt || "Post image";
+    const reactionsFormatted = formatCount(post._count.reactions || 0);
+    const commentsFormatted = formatCount(post._count.comments || 0);
+
+    postElement.innerHTML = `
+        <div class="card">
+            <div class="card-img-top-container w-100 position-relative h-0">
+                <img src="${postMediaUrl}" class="post-image card-img-top position-absolute w-100 h-100 top-0 start-0" alt="${postMediaAlt}">
+            </div>
+            <div class="card-body">
+                <div class="d-flex align-items-center mb-3 text-truncate">
+                    <img src="${
+                      profile.avatar.url || "/images/defaultProfileImage.jpg"
+                    }" class="post-profile-image rounded-circle me-3" alt="${
+      profile.avatar?.alt || "Profile avatar"
+    }">
+                    <div class="d-flex flex-column">
+                        <p class="mb-0 fs-6 fw-light">
+                            Posted by <span class="post-user-name fw-normal">${
+                              profile.name
+                            }</span>
+                        </p>
+                        <p class="card-text fw-light">
+                            <i class="fa-solid fa-heart text-primary"></i>
+                            <span class="post-reactions mx-1">${reactionsFormatted}</span> 
+                            | <span class="post-comments mx-1">${commentsFormatted}</span> comments
+                        </p>
                     </div>
                 </div>
-            </div>`;
-        postContainer.appendChild(postElement);
+            </div>
+        </div>
+      `;
+    postElement.addEventListener("click", () => {
+      window.location.href = `post.html?id=${post.id}`;
     });
+    postContainer.appendChild(postElement);
+  });
 }
