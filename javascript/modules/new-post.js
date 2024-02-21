395f5cf3384911c05f8fe754bd48ab467a840e1a
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2024-02-21 21:48:59 +0100

    edit function finished
    still need error hadnling on post.js and error handling textContent under save changes on editpost

--- javascript/modules/new-post.js
+++ javascript/modules/new-post.js
@@ -13,17 +13,15 @@ document
     const altText = document.getElementById("altText").value;
     const errorFeedback = document.getElementById("postErrorFeedback");
 
-    // Check if "All" is selected, and if so, set tags as an empty array
+    // Check if "Not Specified" is selected, and if so, set tags as an empty array(no tag)
     const selectedContinent = document.getElementById("continentSelect").value;
     const tags = selectedContinent !== "Not Specified" ? [selectedContinent] : [];
 
     if (title.length > 280) {
-      alert("The title cannot be greater than 280 characters.");
       return;
     }
 
     if (body.length > 280) {
-      alert("The post caption cannot be greater than 280 characters.");
       return;
     }
 
@@ -43,7 +41,7 @@ document
     } catch (error) {
       console.error("Failed to create post:", error);
       errorFeedback.textContent =
-        "Failed to create post. Please ensure your image URL is valid, starts with http or https, and that your title and caption do not exceed 280 characters. Please try again.";
+        "Failed to create post. Please ensure your image URL is valid, starts with http or https, and that your title and caption is present and do not exceed 280 characters. Please try again.";
       errorFeedback.style.display = "block";
     }
   });
