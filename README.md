20f578c04f0c9aa9aed1496d1f674a4896cb2f46
Mathias Tinberg <mathias@Mathias-sin-MacBook-Pro.local>
2024-02-28 21:51:00 +0100

    errorhandling for index.js and register.js

--- README.md
+++ README.md
@@ -1,25 +1,9 @@
 # globetrotter
+
 Javascript2 Kopstad and Tinberg
 
-Comments when deliver: 
+Comments when deliver:
+
 - error 404 when deployd to netlify bcs of node_modules is not present. Change the bootstrap css with CDN and remove bootstrap in nodemodules for better preformance!
 
-
-- dato på post 
-
-- error messages finsihed: post.js
-
-
-
-
-
-Alexandra: 
-- ordne layout for post.html (bilde)
-- mer mellomrom på search og filter på explore?
-- explore sort by små skjerm. få all til å være på bunnen også footer på alle sider
-- background color on post.scss se det som er kommentert ut på post.scss
-- på post.html kan title være 280 characters og body text 280 characters, skriv inn 280 inn manuelt i konsollen og finn en måte å få layouten til å passe. 
-- post.html title body og comments blir veldig nærme den grå backgrunnen. vurder å sett mindre margin på "user-post-section" på små skjerm (bare litt mindre)
-- HTML på alle sider med meta description.
-- 
-
+- error messages finsihed: post.js, index.js, register.js
--- javascript/pages/index.js
+++ javascript/pages/index.js
@@ -1,24 +1,43 @@
 //--Import the API Function Login --> modules/api.js --//
-import { loginUser } from '../modules/api.js'; 
+import { loginUser } from "../modules/api.js";
 
 //--Import the JTW Function --> modules/utility.js --//
-import { storeToken } from '../modules/auth.js'; 
+import { storeToken } from "../modules/auth.js";
 
-document.getElementById('loginForm').addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const email = document.getElementById('loginEmail').value;
-  const password = document.getElementById('loginPassword').value;
+//Handles login submissions
+document.addEventListener("DOMContentLoaded", function () {
+  const emailInput = document.getElementById("loginEmail");
+  const passwordInput = document.getElementById("loginPassword");
+  const errorLogin = document.querySelector(".error-login");
+  const successMessage = localStorage.getItem("registrationSuccess");
 
-  try {
-    const result = await loginUser(email, password);
-    console.log(result); 
-    //Store JWT token to local storage --> utility.js
-    storeToken(result.accessToken); 
-    //Store the username to localstorage
-    localStorage.setItem('userName', result.name);
-    window.location.href = `/html/my-profile.html`;
-  } catch (error) {
-    console.error(error.message); 
-    // error code here
+  // Display the success message if it exists in localStorage(from successfully register)
+  if (successMessage) {
+    errorLogin.textContent = successMessage;
+    errorLogin.classList.add("text-success");
+    localStorage.removeItem("registrationSuccess");
   }
+
+  document
+    .getElementById("loginForm")
+    .addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const email = emailInput.value;
+      const password = passwordInput.value;
+
+      try {
+        const result = await loginUser(email, password);
+        // Store JWT token to local storage
+        storeToken(result.accessToken);
+        // Store the username to localstorage
+        localStorage.setItem("userName", result.name);
+        window.location.href = `/html/my-profile.html`;
+      } catch (error) {
+        console.error(error.message);
+        errorLogin.classList.remove("text-success");
+        errorLogin.classList.add("text-danger");
+        errorLogin.textContent =
+          "Login failed. Please ensure your email and password are correct and try again.";
+      }
+    });
 });
